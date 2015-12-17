3fa1cdbf7dbd73b4157a6ebaf795e631d4416349
Takuma Inoue <takumainoue7@gmail.com>
2015-12-17 18:45:40 +0900

    modified

---
+++ git_pull.bat
@@ -0,0 +1,138 @@
+cd ..\設計書
+
+cd 201407-01-bookmanager07
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201407-01-bookmanager07
+git pull
+
+cd ../201407-02-shop
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201407-02-shop
+git pull
+
+cd ../201409-01-bookmanager09
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201409-01-bookmanager09
+git pull
+
+cd ../201410-01-bookmanager
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201410-01-bookmanager
+git pull
+
+cd ../201410-02-ec10
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201410-02-ec10
+git pull
+
+cd ../201411-02-ec11
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201411-02-ec11
+git pull
+
+cd ../201412-01-attendance
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201412-01-attendance
+git pull
+
+cd ../201412-02-chikezon
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201412-02-chikezon
+git pull
+
+cd ../201501-01-oden
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201501-01-oden
+git pull
+
+cd ../201501-01-sakatike
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201501-01-sakatike
+git pull
+
+cd ../201501-02-uesto
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201501-02-uesto
+git pull
+
+cd ../201502-01-tourtike
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201502-01-tourtike
+git pull
+
+cd ../201502-02-solare
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201502-02-solare
+git pull
+
+cd ../201503-01-worldtravel
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201503-01-worldtravel
+git pull
+
+cd ../201503-01-youjustjewelry
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201503-01-youjustjewelry
+git pull
+
+cd ../201503-02-gpscoffee
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201503-02-gpscoffee
+git pull
+
+cd ../201504-01-lapoupee
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201504-01-lapoupee
+git pull
+
+cd ../201504-02-ucycle
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201504-02-ucycle
+git pull
+
+cd ../201505-01-baseballticket
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201505-01-baseballticket
+git pull
+
+cd ../201505-01-liveticket
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201505-01-liveticket
+git pull
+
+cd ../201505-02-ichigoichie
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201505-02-ichigoichie
+git pull
+
+cd ../201506-01-movieticket
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201506-01-movieticket
+git pull
+
+cd ../201506-01-gticket
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201506-01-gticket
+git pull
+
+cd ../201506-02-tbox
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201506-02-tbox
+git pull
+
+cd ../201507-01-biggalaxy
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201507-01-biggalaxy
+git pull
+
+cd ../201507-02-khome
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201507-02-khome
+git pull
+
+cd ../201508-01-aquarium
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201508-01-aquarium
+git pull
+
+cd ../201508-02-aquarium
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201508-02-aquarium
+git pull
+
+cd ../201509-01-lereve
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201509-01-lereve
+git pull
+
+cd ../201509-02-shizuku
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201509-02-shizuku
+git pull
+
+cd ../201510-01-ukiukiutopia
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201510-01-ukiukiutopia
+git pull
+
+cd ../201510-01-footprinter
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201510-01-footprinter
+git pull
+
+cd ../201510-02-pizzaseijin
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201510-02-pizzaseijin
+git pull
+
+cd ../201511-01-cinemajapan
+git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201511-01-cinemajapan
+git pull
+
