7f266cb6524f4a39b26c1c44d146331e794f46a6
Takuma Inoue <takumainoue7@gmail.com>
2015-12-17 19:32:13 +0900

    modified

--- git_push.sh
+++ git_push.sh
@@ -1,205 +1,205 @@
 
 cd 201407-01-bookmanager07
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201407-01-bookmanager07
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201407-01-bookmanager07
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201407-02-shop
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201407-02-shop
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201407-02-shop
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201409-01-bookmanager09
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201409-01-bookmanager09
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201409-01-bookmanager09
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201410-01-bookmanager
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201410-01-bookmanager
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201410-01-bookmanager
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201410-02-ec10
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201410-02-ec10
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201410-02-ec10
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201411-02-ec11
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201411-02-ec11
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201411-02-ec11
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201412-01-attendance
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201412-01-attendance
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201412-01-attendance
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201412-02-chikezon
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201412-02-chikezon
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201412-02-chikezon
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201501-01-oden
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201501-01-oden
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201501-01-oden
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201501-01-sakatike
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201501-01-sakatike
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201501-01-sakatike
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201501-02-uesto
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201501-02-uesto
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201501-02-uesto
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201502-01-tourtike
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201502-01-tourtike
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201502-01-tourtike
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201502-02-solare
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201502-02-solare
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201502-02-solare
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201503-01-worldtravel
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201503-01-worldtravel
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201503-01-worldtravel
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201503-01-youjustjewelry
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201503-01-youjustjewelry
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201503-01-youjustjewelry
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201503-02-gpscoffee
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201503-02-gpscoffee
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201503-02-gpscoffee
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201504-01-lapoupee
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201504-01-lapoupee
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201504-01-lapoupee
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201504-02-ucycle
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201504-02-ucycle
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201504-02-ucycle
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201505-01-baseballticket
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201505-01-baseballticket
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201505-01-baseballticket
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201505-01-liveticket
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201505-01-liveticket
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201505-01-liveticket
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201505-02-ichigoichie
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201505-02-ichigoichie
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201505-02-ichigoichie
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201506-01-movieticket
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201506-01-movieticket
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201506-01-movieticket
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201506-01-gticket
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201506-01-gticket
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201506-01-gticket
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201506-02-tbox
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201506-02-tbox
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201506-02-tbox
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201507-01-biggalaxy
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201507-01-biggalaxy
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201507-01-biggalaxy
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201507-02-khome
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201507-02-khome
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201507-02-khome
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201508-01-aquarium
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201508-01-aquarium
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201508-01-aquarium
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201508-02-aquarium
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201508-02-aquarium
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201508-02-aquarium
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201509-01-lereve
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201509-01-lereve
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201509-01-lereve
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201509-02-shizuku
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201509-02-shizuku
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201509-02-shizuku
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201510-01-ukiukiutopia
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201510-01-ukiukiutopia
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201510-01-ukiukiutopia
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201510-01-footprinter
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201510-01-footprinter
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201510-01-footprinter
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201510-02-pizzaseijin
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201510-02-pizzaseijin
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201510-02-pizzaseijin
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
 cd ../201511-01-cinemajapan
-git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201511-01-cinemajapan
-git pull
-git add --all
-git commit -m "modified"
-git push
+sudo git remote set-url origin https://internousdevdoc:aUTRCLT3@github.com/internousdevdoc/201511-01-cinemajapan
+sudo git pull
+sudo git add --all
+sudo git commit -m "modified"
+sudo git push
